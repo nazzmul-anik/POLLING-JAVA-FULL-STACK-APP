8b8501462d7daf8c4baccaa33d75b499947df6a6
Md. Nazmul Hasan Anik <nazzmul.anik@gmail.com>
2026-04-10 01:54:02 +0600

    Create and design Sign Up and Sign In page.

--- polling-react/src/pages/auth/login/Login.jsx
+++ polling-react/src/pages/auth/login/Login.jsx
@@ -1,107 +1,132 @@
-import { Email } from '@mui/icons-material'
-import React, { useState } from 'react'
-import { Navigate, useNavigate } from 'react-router-dom';
-import {Avatar, Backdrop, Typography, Container, TextField, Button, CircularProgress, Grid, CssBaseline, Box, Link} from "@mui/material";
-import {createTheme, ThemeProvider} from '@mui/material/styles'
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import { Email } from "@mui/icons-material";
+import React, { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import {
+  Avatar,
+  Backdrop,
+  Typography,
+  Container,
+  TextField,
+  Button,
+  CircularProgress,
+  Grid,
+  CssBaseline,
+  Box,
+  Link,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 const defaultTheme = createTheme();
 const Login = () => {
-  const[formData, setFormData] = useState({
-    email: '',
-    password: ''
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
   });
-
-  const[loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
-  const handleInputChange = async(e)=>{
-    const {name, value} = e.target;
+  const handleInputChange = async (e) => {
+    const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
-    })
+      [name]: value,
+    });
   };
-  const handleSubmit = async(e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
     console.log(formData);
     setLoading(false);
-  }
+  };
   return (
     <>
-        <ThemeProvider theme={defaultTheme}>
-          <Container component="main" maxWidth="xs">
-            <CssBaseline/>
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sr={{ m: 1, bgcolor: "primary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
             <Box
-              sx={{
-                marginTop: 8,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center'
-              }}>
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={formData.email}
+                onChange={handleInputChange}
+              />
 
-                <Avatar sr={{m: 1, bgcolor: 'primary.main'}}>
-                  <LockOutlinedIcon/>
-                </Avatar>
-                <Typography component="h1" variant='h5'>
-                  Sign in
-                </Typography>
-                <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
-                  <TextField
-                    margin='normal'
-                    required
-                    fullWidth
-                    id='email'
-                    label='Email Address'
-                    name='email'
-                    autoComplete='email'
-                    autoFocus
-                    value={formData.email}
-                    onChange={handleInputChange}
-                  />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="passoword"
+                id="password"
+                autoComplete="current-password"
+                value={formData.password}
+                onChange={handleInputChange}
+              />
 
-                  <TextField
-                    margin='normal'
-                    required
-                    fullWidth
-                    name='password'
-                    label='Password'
-                    type='passoword'
-                    id='password'
-                    autoComplete='current-password'
-                    value={formData.password}
-                    onChange={handleInputChange}
-                  />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                disabled={!formData.email || !formData.password}
+              >
+                {loading ? (
+                  <CircularProgress color="success" size={24} />
+                ) : (
+                  "Sign In"
+                )}
+              </Button>
 
-                  <Button
-                    type='submit'
-                    fullWidth
-                    variant='contained'
-                    sx={{mt: 3, mb:2}}
-                    disabled={!formData.email || !formData.password}
+              <Grid container>
+                <Grid>
+                  <Link
+                    style={{ cursor: "pointer" }}
+                    variant="body2"
+                    onClick={() => navigate("/register")}
                   >
-                    {loading ? <CircularProgress color='success' size={24}/>: 'Sign In'}
-                  </Button>
-
-                  <Grid Container>
-                    <Grid item>
-                      <Link variant="body2" onClick={()=> navigate("/register")}>
-                        {"Don't have an account? Sign Up"}
-                      </Link>
-                    </Grid>
-                  </Grid>
-                </Box>
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
             </Box>
-          </Container>
-        </ThemeProvider>
-        <Backdrop
-        sx={{color: '#fff', zIndex: (theme)=> theme.zIndex.drawer+1}}
-        open={loading}>
-          <CircularProgress color="success"/>
-        </Backdrop>
+          </Box>
+        </Container>
+      </ThemeProvider>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={loading}
+      >
+        <CircularProgress color="success" />
+      </Backdrop>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- polling-react/src/pages/auth/signup/Signup.jsx
+++ polling-react/src/pages/auth/signup/Signup.jsx
@@ -1,11 +1,176 @@
-import React from 'react'
+import { Email } from "@mui/icons-material";
+import React, { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import {
+  Avatar,
+  Backdrop,
+  Typography,
+  Container,
+  TextField,
+  Button,
+  CircularProgress,
+  Grid,
+  CssBaseline,
+  Box,
+  Link,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
+const defaultTheme = createTheme();
 const Signup = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleInputChange = async (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    console.log(formData);
+    setLoading(false);
+  };
+
   return (
     <>
-        <h2>Signup Works!</h2>
-    </>
-  )
-}
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sr={{ m: 1, bgcolor: "primary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign Up
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 3 }}
+            >
+              <Grid container spacing={2}>
+                <Grid size={{ xs: 12, sm: 6 }}>
+                  <TextField
+                    margin="normal"
+                    autoComplete="given-name"
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firtName"
+                    label="First Name"
+                    autoFocus
+                    value={formData.firstName}
+                    onChange={handleInputChange}
+                  />
+                </Grid>
 
-export default Signup
+                <Grid size={{ xs: 12, sm: 6 }}>
+                  <TextField
+                    margin="normal"
+                    autoComplete="family-name"
+                    name="lastName"
+                    required
+                    fullWidth
+                    id="firtName"
+                    label="Last Name"
+                    autoFocus
+                    value={formData.lastName}
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+
+                <Grid size={{ xs: 12 }}>
+                  <TextField
+                    margin="normal"
+                    autoComplete="email"
+                    name="email"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    autoFocus
+                    value={formData.email}
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+
+                <Grid size={{ xs: 12 }}>
+                  <TextField
+                    margin="normal"
+                    autoComplete="new-password"
+                    name="password"
+                    required
+                    fullWidth
+                    id="password"
+                    label="Password"
+                    autoFocus
+                    value={formData.password}
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                  disabled={
+                    !formData.email ||
+                    !formData.password ||
+                    !formData.lastName ||
+                    !formData.firstName
+                  }
+                >
+                  {loading ? (
+                    <CircularProgress color="success" size={24} />
+                  ) : (
+                    "Sign Up"
+                  )}
+                </Button>
+
+                <Grid container justifyContent="flex-end">
+                  <Grid>
+                    <Link
+                      style={{ cursor: "pointer" }}
+                      variant="body2"
+                      onClick={() => navigate("/login")}
+                    >
+                      {"Already have an account? Sign In"}
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Container>
+      </ThemeProvider>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={loading}
+      >
+        <CircularProgress color="success" />
+      </Backdrop>
+    </>
+  );
+};
+
+export default Signup;
