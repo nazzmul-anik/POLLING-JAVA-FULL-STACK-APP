4a6cfd53161a3b67a4492e898d1e6911e2ae9a26
Md. Nazmul Hasan Anik <nazzmul.anik@gmail.com>
2026-04-12 17:44:06 +0600

    Change login status code.

--- Polling-Spring-Boot/src/main/java/com/nazmul/polling/controller/auth/AuthController.java
+++ Polling-Spring-Boot/src/main/java/com/nazmul/polling/controller/auth/AuthController.java
@@ -3,20 +3,17 @@ package com.nazmul.polling.controller.auth;
 import com.nazmul.polling.dto.AuthenticationRequest;
 import com.nazmul.polling.dto.AuthenticationResponse;
 import com.nazmul.polling.dto.SignupRequest;
-import com.nazmul.polling.dto.UserDTO;
 import com.nazmul.polling.services.auth.AuthService;
 import com.nazmul.polling.services.jwt.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class AuthController {
 
     private final AuthService authService;
@@ -31,6 +28,6 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@RequestBody AuthenticationRequest authenticationRequest){
         AuthenticationResponse response = authService.login(authenticationRequest);
-        return new ResponseEntity<>(response, HttpStatus.FOUND);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
