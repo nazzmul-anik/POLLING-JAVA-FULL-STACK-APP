0afa824f61f04393976c76f30ae269c46e2a9e61
Md. Nazmul Hasan Anik <nazzmul.anik@gmail.com>
2026-04-12 19:56:28 +0600

    Write frontend token wise code. Means is user token are valid or not or login or logout, that basis showing UI.

--- polling-react/src/pages/header/Header.jsx
+++ polling-react/src/pages/header/Header.jsx
@@ -1,35 +1,91 @@
-import React from 'react'
-import {AppBar, Box, Toolbar, IconButton, Typography, Button} from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
-import {Link} from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { isTokenValid, removeToken } from "../../utility/Common";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+
+  const handleSignOut = () => {
+    navigate("/login");
+    removeToken();
+  };
+
+  useEffect(() => {
+    const isLoggedIn = isTokenValid();
+    setIsUserLoggedIn(isLoggedIn);
+  }, [location]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!isTokenValid()) {
+        setIsUserLoggedIn(false);
+        handleSignOut();
+      }
+    }, 1800000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
-        <Box sx={{flexGrow: 1}}>
-            <AppBar postion="static">
-                <Toolbar>
-                  <IconButton
-                    size="large"
-                    edge="start"
-                    color="inherit"
-                    aria-label="menu"
-                    sx={{mr:2}}
-                  >
-                    <MenuIcon/>
-                  </IconButton>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar postion="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
 
-                  <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-                    Polling App
-                  </Typography>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Polling App
+            </Typography>
 
-                  <Button component={Link} to="/login" color="inherit">Login</Button>
-                  <Button component={Link} to="/register" color="inherit">Register</Button>
-                </Toolbar>
-            </AppBar>
-        </Box>
+            {isUserLoggedIn ? (
+              <>
+                <Button component={Link} to="/dashboard" color="inherit">
+                  Dashboard
+                </Button>
+                <Button component={Link} to="/poll" color="inherit">
+                  Post Poll
+                </Button>
+                <Button component={Link} to="/my-polls" color="inherit">
+                  My Polls
+                </Button>
+                <Button onClick={handleSignOut} color="inherit">
+                  Logout
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button component={Link} to="/login" color="inherit">
+                  Login
+                </Button>
+                <Button component={Link} to="/register" color="inherit">
+                  Register
+                </Button>
+              </>
+            )}
+          </Toolbar>
+        </AppBar>
+      </Box>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- polling-react/src/utility/Common.jsx
+++ polling-react/src/utility/Common.jsx
@@ -1,6 +1,37 @@
+import { Token } from "@mui/icons-material";
 import Cookies from "js-cookie";
+import { jwtDecode } from "jwt-decode";
 
 const TOKEN = "token";
+
 export const saveToken = (token) => {
   Cookies.set(TOKEN, token);
+};
+
+export const getToken = () => {
+  return Cookies.get(TOKEN) || null;
+};
+
+export const removeToken = () => {
+  Cookies.remove(TOKEN);
+};
+
+export const decodeToken = () => {
+  const token = getToken();
+  if (!token) return null;
+
+  try {
+    return jwtDecode(token);
+  } catch (error) {
+    console.error("Failed to decode token: ", error);
+    return null;
+  }
+};
+
+export const isTokenValid = () => {
+  const deToken = decodeToken();
+  if (!deToken || !deToken.exp) return false;
+
+  const expiry = deToken.exp * 1000;
+  return Date.now() < expiry;
 };
