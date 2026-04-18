75313cd726bf3068e3822120ed7226279ac23d03
Md. Nazmul Hasan Anik <nazzmul.anik@gmail.com>
2026-04-18 13:09:39 +0600

    Fixup frontend api endpoints.

--- polling-react/src/services/poll/Poll.jsx
+++ polling-react/src/services/poll/Poll.jsx
@@ -1,44 +1,44 @@
 import axiosInstance from "../../environment/AxiosInstance";
 
 export const postPoll = async (pollDTO) => {
-  const response = await axiosInstance.post("/api/user/poll", pollDTO);
+  const response = await axiosInstance.post("/api/user/polls/", pollDTO);
   return response;
 };
 
 export const getAllPolls = async () => {
-  const response = await axiosInstance.get("/api/user/polls");
+  const response = await axiosInstance.get("/api/user/polls/");
   return response;
 };
 
 export const getMyPolls = async () => {
-  const response = await axiosInstance.get("/api/user/my-polls");
+  const response = await axiosInstance.get("/api/user/polls/my-polls");
   return response;
 };
 
 export const deletePollById = async (id) => {
-  const response = await axiosInstance.delete(`/api/user/poll/${id}`);
+  const response = await axiosInstance.delete(`/api/user/polls/${id}`);
   return response;
 };
 
 export const getPollById = async (id) => {
-  const response = await axiosInstance.get(`/api/user/poll/${id}`);
+  const response = await axiosInstance.get(`/api/user/polls/${id}`);
   return response;
 };
 
 export const giveLikeToPoll = async (pollId) => {
-  const response = await axiosInstance.get(`/api/user/poll/like/${pollId}`);
+  const response = await axiosInstance.get(`/api/user/polls/like/${pollId}`);
   return response;
 };
 
 export const postCommentOnPoll = async (commentDTO) => {
   const response = await axiosInstance.post(
-    "/api/user/poll/comment",
+    "/api/user/polls/comment",
     commentDTO,
   );
   return response;
 };
 
 export const postVoteOnPoll = async (voteDTO) => {
-  const response = await axiosInstance.post("/api/user/poll/vote", voteDTO);
+  const response = await axiosInstance.post("/api/user/polls/vote", voteDTO);
   return response;
 };
